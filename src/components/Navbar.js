2b0fd4f4be41f3552d6687385b1262560f918632
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-12-08 00:11:59 +0530

    Added Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,10 @@ export default function Navbar() {
     <div className='navbar'>
       <ul>
         <li className='logo'>
-          <img src={Temple} alt='dojo logo' />
-          <span>TheDojoMass</span>
+          <Link to='/'>
+            <img src={Temple} alt='dojo logo' />
+            <span>TheDojoMass</span>
+          </Link>
         </li>
         <li>
           <Link to='/login'>Login</Link>
